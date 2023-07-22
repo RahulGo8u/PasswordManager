7b6dee4d1a19853b86d74338cd227253fc63a2af
Rahul Anand <44201543+RahulGo8u@users.noreply.github.com>
2023-07-23 01:22:35 +0400

    Delete password data array

--- client/src/data/password.js
+++
@@ -1,7 +0,0 @@
-const passwords = [
-  { id: 1, title: 'Password 1', username: 'user1', password: 'password1' },
-  { id: 2, title: 'Password 2', username: 'user2', password: 'password2' },
-  // Add more passwords as needed
-];
-
-export default passwords;
